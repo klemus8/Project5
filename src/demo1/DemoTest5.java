4866cd35d0fe086de6bf214fbd5c9e9097a48aa7
karla <karla@192.168.1.154>
2025-01-29 19:58:37 -0600

    implemented add()

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -2,7 +2,12 @@ package demo1;
 
 public class DemoTest5 {
 	public static void main(String[] args) {
-		System.out.println("Java");
+		System.out.println("KarlaLemus: Git Lecture");
 		
 }
+	
+	public void add(int a, int b) {
+		System.out.println(a + b);
+		
+	}
 }
