0edb30a2191d27ddf38f18d2c2a3cfe85217e9be
klemus8 <klemush8@gmail.com>
2025-01-29 20:23:29 -0600

    Update DemoTest5.java
    TEST

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -3,6 +3,25 @@ package demo1;
 public class DemoTest5 {
 	public static void main(String[] args) {
 		System.out.println("KarlaLemus: Git Lecture");
+
+		int a = 10;
+		int b =20;
+
+		int result = a + b;
+
+		System.out.println(result);
+
+		int x = 10;
+		int y = 20;
+
+		if(x > y) {
+			System.out.println("Greater");
+		} else {
+			System.out.println("Less");
+		}
+
+		//This is coming from github
+		//Practicing pulling
 		
 }
 	
